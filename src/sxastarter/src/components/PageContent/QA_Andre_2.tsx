3c1fd81bcb7ec5d41d929f3ef7b3b86c8bc77f2e
Andre Ong <ong@sitecore.net>
2025-10-14 14:26:36 +0800

    updated qa andre variant

--- src/sxastarter/src/components/PageContent/QA_Andre_2.tsx
+++ src/sxastarter/src/components/PageContent/QA_Andre_2.tsx
@@ -313,178 +313,274 @@ export const Default = (props: QAProps): JSX.Element => {
 };
 
 export const PrettierByAI = (props: QAProps): JSX.Element => {
-  const id = props.params.RenderingIdentifier;
-  const fieldBlock = (title: string, content: JSX.Element) => (
-    <div className="tile bg-gray-100 rounded-md shadow-md p-4 mb-4">
-      <h3 className="tile-title text-lg font-bold mb-2">{title}</h3>
-      <div>{content}</div>
-    </div>
-  );
-
-  return props.fields ? (
+  return (
     <div
-      className={`component variant-grid ${props?.params?.styles?.trimEnd()}`}
-      id={id ? id : undefined}
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+        gap: '1rem',
+      }}
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        {fieldBlock(
-          'Single Line Text',
-          <>
-            <Text field={props.fields?.SingleLine1} />
-            <br />
-            <Text field={props.fields?.SingleLine2} />
-          </>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Single-Line Text</h2>
+        {props.fields.SingleLine1?.value && <Text field={props.fields.SingleLine1} />}
+        {props.fields.SingleLine2?.value && <Text field={props.fields.SingleLine2} />}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Multi-Line Text</h2>
+        {props.fields.MultiLine1?.value && <Text field={props.fields.MultiLine1} />}
+        {props.fields.MultiLine2?.value && <Text field={props.fields.MultiLine2} />}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Rich Text</h2>
+        {props.fields.RichText1?.value && <RichText field={props.fields.RichText1} />}
+        {props.fields.RichText2?.value && <RichText field={props.fields.RichText2} />}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Integer</h2>
+        {props.fields.Integer1?.value !== undefined && (
+          <Text field={{ value: String(props.fields.Integer1?.value) }} />
         )}
-        {fieldBlock(
-          'Multi Line Text',
-          <>
-            <Text field={props.fields?.MultiLine1} />
-            <br />
-            <Text field={props.fields?.MultiLine2} />
-          </>
-        )}
-        {fieldBlock(
-          'Rich Text',
-          <>
-            <RichText field={props.fields?.RichText1} />
-            <RichText field={props.fields?.RichText2} />
-          </>
-        )}
-        {fieldBlock(
-          'Integer',
-          <>
-            <Text field={props.fields?.Integer1} />
-            <br />
-            <Text field={props.fields?.Integer2} />
-          </>
-        )}
-        {fieldBlock(
-          'Number',
-          <>
-            <Text field={props.fields?.Number1} />
-            <br />
-            <Text field={props.fields?.Number2} />
-          </>
-        )}
-        {fieldBlock(
-          'Date',
-          <>
-            <Text field={props.fields?.Date1} />
-            <br />
-            <Text field={props.fields?.Date2} />
-          </>
-        )}
-        {fieldBlock(
-          'Datetime',
-          <>
-            <Text field={props.fields?.DateTime1} />
-            <br />
-            <Text field={props.fields?.DateTime2} />
-          </>
-        )}
-        {fieldBlock(
-          'Multilist',
-          <>
-            {Array.isArray(props.fields.MultiList1.value) &&
-              props.fields.MultiList1.value.map((item: { label: string }) => item.label).join(', ')}
-            <br />
-            {Array.isArray(props.fields.MultiList2.value) &&
-              props.fields.MultiList2.value.map((item: { label: string }) => item.label).join(', ')}
-          </>
-        )}
-        {fieldBlock(
-          'Multilist with Search',
-          <>
-            {Array.isArray(props.fields.MultiListSearch1.value) &&
-              props.fields.MultiListSearch1.value
-                .map((item: { label: string }) => item.label)
-                .join(', ')}
-            <br />
-            {Array.isArray(props.fields.MultiListSearch2.value) &&
-              props.fields.MultiListSearch2.value
-                .map((item: { label: string }) => item.label)
-                .join(', ')}
-          </>
-        )}
-        {fieldBlock(
-          'Tree List',
-          <>
-            {Array.isArray(props.fields.TreeList1.value) &&
-              props.fields.TreeList1.value.map((item: { label: string }) => item.label).join(', ')}
-            <br />
-            {Array.isArray(props.fields.TreeList2.value) &&
-              props.fields.TreeList2.value.map((item: { label: string }) => item.label).join(', ')}
-          </>
-        )}
-        {fieldBlock(
-          'Multiroot Tree List',
-          <>
-            {Array.isArray(props.fields.MultiRootTreeList1.value) &&
-              props.fields.MultiRootTreeList1.value
-                .map((item: { label: string }) => item.label)
-                .join(', ')}
-            <br />
-            {Array.isArray(props.fields.MultiRootTreeList2.value) &&
-              props.fields.MultiRootTreeList2.value
-                .map((item: { label: string }) => item.label)
-                .join(', ')}
-          </>
-        )}
-        {fieldBlock(
-          'Checklist',
-          <>
-            {Array.isArray(props.fields.CheckList1.value) &&
-              props.fields.CheckList1.value.map((item: { label: string }) => item.label).join(', ')}
-            <br />
-            {Array.isArray(props.fields.CheckList2.value) &&
-              props.fields.CheckList2.value.map((item: { label: string }) => item.label).join(', ')}
-          </>
-        )}
-        {fieldBlock(
-          'Tag List',
-          <>
-            {Array.isArray(props.fields.TagList1.value) &&
-              props.fields.TagList1.value.map((item: { label: string }) => item.label).join(', ')}
-            <br />
-            {Array.isArray(props.fields.TagList2.value) &&
-              props.fields.TagList2.value.map((item: { label: string }) => item.label).join(', ')}
-          </>
-        )}
-        {fieldBlock(
-          'Checkbox',
-          <>
-            <Text field={props.fields.CheckBox1} />
-            <br />
-            <Text field={props.fields.CheckBox2} />
-          </>
-        )}
-        {fieldBlock(
-          'General Link',
-          <>
-            <Link field={props.fields.Link1}>{props.fields.Link1.value?.text || 'Link 1'}</Link>
-            <br />
-            <Link field={props.fields.Link2}>{props.fields.Link2.value?.text || 'Link 2'}</Link>
-          </>
-        )}
-        {fieldBlock(
-          'Droptree',
-          <>
-            <span>Tree 1: {props.fields.DropTree1?.value?.label}</span>
-            <br />
-            <span>Tree 2: {props.fields.DropTree2?.value?.label}</span>
-          </>
-        )}
-        {fieldBlock(
-          'Droplink',
-          <>
-            <span>Link 1: {props.fields.DropLink1?.value?.label}</span>
-            <br />
-            <span>Link 2: {props.fields.DropLink2?.value?.label}</span>
-          </>
+        {props.fields.Integer2?.value !== undefined && (
+          <Text field={{ value: String(props.fields.Integer2?.value) }} />
         )}
       </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Number</h2>
+        {props.fields.Number1?.value !== undefined && (
+          <Text field={{ value: String(props.fields.Number1?.value) }} />
+        )}
+        {props.fields.Number2?.value !== undefined && (
+          <Text field={{ value: String(props.fields.Number2?.value) }} />
+        )}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Date</h2>
+        {props.fields.Date1?.value && <Text field={props.fields.Date1} />}
+        {props.fields.Date2?.value && <Text field={props.fields.Date2} />}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>DateTime</h2>
+        {props.fields.DateTime1?.value && <Text field={props.fields.DateTime1} />}
+        {props.fields.DateTime2?.value && <Text field={props.fields.DateTime2} />}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>General Links</h2>
+        {props.fields.Link1?.value && (
+          <Link field={props.fields.Link1}>{props.fields.Link1?.value?.text}</Link>
+        )}
+        {props.fields.Link2?.value && (
+          <Link field={props.fields.Link2}>{props.fields.Link2?.value?.text}</Link>
+        )}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Drop List</h2>
+        {props.fields.DropList1?.value && <Text field={props.fields.DropList1} />}
+        {props.fields.DropList2?.value && <Text field={props.fields.DropList2} />}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Drop Link</h2>
+        <span>{props.fields.DropLink1?.value?.label}</span>
+        <span>{props.fields.DropLink2?.value?.label}</span>
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Drop Tree</h2>
+        <span>{props.fields.DropTree1?.value?.label}</span>
+        <span>{props.fields.DropTree2?.value?.label}</span>
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Multi List</h2>
+        {Array.isArray(props.fields.MultiList1.value)
+          ? props.fields.MultiList1.value.map((item: { label: string }) => item?.label).join(', ')
+          : ''}
+        {Array.isArray(props.fields.MultiList2.value)
+          ? props.fields.MultiList2.value.map((item: { label: string }) => item?.label).join(', ')
+          : ''}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Multi List Search</h2>
+        {Array.isArray(props.fields.MultiListSearch1.value)
+          ? props.fields.MultiListSearch1.value
+              .map((item: { label: string }) => item?.label)
+              .join(', ')
+          : ''}
+        {Array.isArray(props.fields.MultiListSearch2.value)
+          ? props.fields.MultiListSearch2.value
+              .map((item: { label: string }) => item?.label)
+              .join(', ')
+          : ''}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Tree List</h2>
+        {Array.isArray(props.fields.TreeList1.value)
+          ? props.fields.TreeList1.value.map((item: { label: string }) => item?.label).join(', ')
+          : ''}
+        {Array.isArray(props.fields.TreeList2.value)
+          ? props.fields.TreeList2.value.map((item: { label: string }) => item?.label).join(', ')
+          : ''}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Multiroot Tree List</h2>
+        {Array.isArray(props.fields.MultiRootTreeList1.value)
+          ? props.fields.MultiRootTreeList1.value
+              .map((item: { label: string }) => item?.label)
+              .join(', ')
+          : ''}
+        {Array.isArray(props.fields.MultiRootTreeList2.value)
+          ? props.fields.MultiRootTreeList2.value
+              .map((item: { label: string }) => item?.label)
+              .join(', ')
+          : ''}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Check List</h2>
+        {Array.isArray(props.fields.CheckList1.value)
+          ? props.fields.CheckList1.value.map((item: { label: string }) => item?.label).join(', ')
+          : ''}
+        {Array.isArray(props.fields.CheckList2.value)
+          ? props.fields.CheckList2.value.map((item: { label: string }) => item?.label).join(', ')
+          : ''}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Tag List</h2>
+        {Array.isArray(props.fields.TagList1.value)
+          ? props.fields.TagList1.value.map((item: { label: string }) => item?.label).join(', ')
+          : ''}
+        {Array.isArray(props.fields.TagList2.value)
+          ? props.fields.TagList2.value.map((item: { label: string }) => item?.label).join(', ')
+          : ''}
+      </div>
+      <div
+        style={{
+          padding: '0.5rem',
+          backgroundColor: '#f7f7f7',
+          border: '1px solid #ccc',
+          borderRadius: '0.5rem',
+        }}
+      >
+        <h2>Check Box</h2>
+        {props.fields.CheckBox1?.value && <Text field={props.fields.CheckBox1} />}
+        {props.fields.CheckBox2?.value && <Text field={props.fields.CheckBox2} />}
+      </div>
     </div>
-  ) : (
-    <ComponentDefault {...props} />
   );
 };
